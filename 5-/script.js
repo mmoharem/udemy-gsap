e84aa37a59b6520e5e589d253952d5216d0ce26d
mohamed moharram <cloud@bnfstore.com>
2018-10-16 11:57:12 +0200

    cont 4-

--- 5-/script.js
+++ 5-/script.js
@@ -1,11 +1,6 @@
-// TweenLite.to('#logo1', 1, {y:800});
-// TweenLite.to('#logo2',  1, {y:800, delay:1/3});
-// TweenLite.to('#logo3', 1, {y:800, delay:2/3});
+// Using absolute postion Reference, relative position ref
 
-// /Using Array
-// TweenMax.to(['#logo1', '#logo2', '#logo3'], 1, {y:800});
-
-// Using Stagger
-// TweenMax.staggerTo(['#logo1', '#logo2', '#logo3'], 1, {y:800}, 0.3)
-// TweenMax.staggerFrom(['#logo1', '#logo2', '#logo3'], 1, {y:800}, 0.3)
-// TweenMax.staggerFromTo(['#logo1', '#logo2', '#logo3'], 1, {y:350}, {y:800}, 0.3)
+var masterT1 = new TimelineMax();
+masterT1.to('#logo1', 1, {x:850})
+        .to('#logo2', 1, {y:830}, '-=0.5')
+        .to('#logo3', 1, {x:'-=850'}, '-=0.5');
